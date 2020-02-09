3b57040b0412168fcc552aba7d8feb51e371d7e8
Julien Perret <julien.perret@gmail.com>
2020-02-09 12:36:32 +0100

    adding a PopulationGeopackageExporter in order to export large synthetic populations (french population)

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ sbt "runMain eighties.h24.tools.ExtractRelevantData -c data/CONTOURS-IRIS_2-0__S
 ```
 The following command extracts the data for the entire french territory:
 ```shell script
-sbt -J-Xmx8G "runMain eighties.h24.tools.PopulationGenerator -c prepared_data/CONTOURS-IRIS_FE.shp -g prepared_data/R_rfl09_LAEA1000.shp -s 1000 -p prepared_data/base-ic-evol-struct-pop-2012.csv.lzma -f prepared_data/base-ic-diplomes-formation-2012.csv.lzma -o results/population.bin"
+sbt "runMain eighties.h24.tools.ExtractRelevantData -c data/CONTOURS-IRIS_2-0__SHP_LAMB93_FXX_2014-01-01/CONTOURS-IRIS/1_DONNEES_LIVRAISON_2014/CONTOURS-IRIS_2-0_SHP_LAMB93_FE-2014/CONTOURS-IRIS_FE.shp -g data/GRID/R_rfl09_LAEA1000.shp -p data/base-ic-evol-struct-pop-2012.xls -f data/base-ic-diplomes-formation-2012.xls -o prepared_data"
 ```
 
 You get the idea, right?
@@ -61,11 +61,23 @@ For Île-de-France (note we added a JVM option to give more memory to the proces
 ```shell script
 sbt -J-Xmx4G "runMain eighties.h24.tools.PopulationGenerator -c prepared_data_IDF/CONTOURS-IRIS_FE.shp -g prepared_data_IDF/R_rfl09_LAEA1000.shp -s 1000 -p prepared_data_IDF/base-ic-evol-struct-pop-2012.csv.lzma -f prepared_data_IDF/base-ic-diplomes-formation-2012.csv.lzma -o results_IDF/population.bin"
 ```
+For the entire french territory:
+```shell script
+sbt -J-Xmx8G "runMain eighties.h24.tools.PopulationGenerator -c prepared_data/CONTOURS-IRIS_FE.shp -g prepared_data/R_rfl09_LAEA1000.shp -s 1000 -p prepared_data/base-ic-evol-struct-pop-2012.csv.lzma -f prepared_data/base-ic-diplomes-formation-2012.csv.lzma -o results/population.bin"
+```
+### Export you synthetic population!
 You can also export the population as a shapefile to see how it looks like.
 For Île-de-France (note we added a JVM option to give more memory to the process):
 ```shell script
 sbt -J-Xmx4G "runMain eighties.h24.tools.PopulationShapefileExporter -p results_IDF/population.bin -o results_IDF/population.shp"
 ```
+
+For the entire french territory, a shapefile will not work, you can use the geopackage exporter:
+```shell script
+sbt -J-Xmx8G "runMain eighties.h24.tools.PopulationGeopackageExporter -p results/population.bin -o results/population.gpkg"
+```
+And now, you have a french synthetic population!
+![A french synthetic population map](images/population_heatmap.png "A french synthetic population")
 
 ## Let's use Origin-Destination data
 
---
+++ src/main/scala/eighties/h24/tools/PopulationGeopackageExporter.scala
@@ -0,0 +1,106 @@
+package eighties.h24.tools
+
+import java.io.{File => JFile}
+import java.util
+
+import eighties.h24.generation._
+import org.geotools.data.{DataUtilities, DefaultTransaction}
+import org.geotools.geometry.jts.{Geometries, ReferencedEnvelope}
+import org.geotools.geopkg.FeatureEntry
+import org.geotools.referencing.CRS
+import org.locationtech.jts.geom.{Coordinate, GeometryFactory}
+import scopt.OParser
+
+object PopulationGeopackageExporter extends App {
+  case class Config(
+                     population: Option[JFile] = None,
+                     output: Option[JFile] = None
+                   )
+
+  val builder = OParser.builder[Config]
+  val parser = {
+    import builder._
+    OParser.sequence(
+      programName("population export as shapefile"),
+      opt[java.io.File]('p', "population")
+        .required()
+        .action((x, c) => c.copy(population = Some(x)))
+        .text("population file generated with h24"),
+      opt[java.io.File]('o', "output")
+        .required()
+        .action((x, c) => c.copy(output = Some(x)))
+        .text("output directory")
+    )
+  }
+  OParser.parse(parser, args, Config()) match {
+    case Some(config) =>
+      Log.log("load features")
+      val res = WorldFeature.load(config.population.get)
+      val bbox = res.originalBoundingBox
+      val gridSize = res.gridSize
+      val outFile = config.output.get
+      outFile.getParentFile.mkdirs()
+      val params = new util.HashMap[String, Object]()
+      params.put("dbtype", "geopkg")
+      params.put("database", outFile.getPath)
+      val specs = "geom:Point:srid=3035," +
+        "cellX:Integer," +
+        "cellY:Integer," +
+        "ageCat:Integer," +
+        "sex:Integer," +
+        "education:Integer"
+      val featureTypeName = "GeoPKG"
+      val featureType = DataUtilities.createType(featureTypeName, specs)
+      val geometryFactory = new GeometryFactory
+      import org.geotools.geopkg.GeoPackage
+      Log.log("create geopackage")
+      val geopkg = new GeoPackage(outFile)
+      Log.log("init geopackage")
+      geopkg.init()
+      Log.log("create geopackage")
+      val entry = new FeatureEntry
+      entry.setGeometryColumn (featureType.getGeometryDescriptor.getLocalName)
+      val geometryType = featureType.getGeometryDescriptor.getType
+      val gType = Geometries.getForName (geometryType.getName.getLocalPart)
+      entry.setGeometryType (gType)
+      val referencedEnvelope = new ReferencedEnvelope(bbox.minI, bbox.maxI+gridSize, bbox.minJ, bbox.maxJ+gridSize, CRS.decode("EPSG:3035"))
+      entry.setBounds(referencedEnvelope)
+      Log.log("create featureentry")
+      geopkg.create(entry, featureType)
+      val transaction = new DefaultTransaction()
+      Log.log("create writer")
+      val writer = geopkg.writer(entry,true, null, transaction)
+      Log.log("Let's go")
+      // FIXME the index if for debug only: we might want to remove it later
+      var index = 0
+      for {
+        feature <- res.individualFeatures
+      } {
+        if (index % 10000 == 0) Log.log(s"$index")
+        import feature._
+        def point = geometryFactory.createPoint(new Coordinate(bbox.minI + location._1.toDouble * gridSize + gridSize / 2, bbox.minJ + location._2.toDouble * gridSize + gridSize / 2))
+        val values = Array[AnyRef](
+          point,
+          location._1.asInstanceOf[AnyRef],
+          location._2.asInstanceOf[AnyRef],
+          ageCategory.asInstanceOf[AnyRef],
+          sex.asInstanceOf[AnyRef],
+          education.asInstanceOf[AnyRef]
+        )
+        def simpleFeature = writer.next
+        simpleFeature.setAttributes(values)
+        writer.write()
+        index += 1
+      }
+      Log.log("close writer")
+      writer.close()
+      Log.log("commit transaction")
+      transaction.commit()
+      Log.log("close transaction")
+      transaction.close()
+      Log.log("close geopackage")
+      geopkg.close()
+      Log.log("all done")
+    case _ =>
+  }
+}
