31252e9a3e0ea6eaed5726ed6985b7573086035b
Julien Perret <julien.perret@gmail.com>
2021-05-18 22:44:50 +0200

    adding the option to export the population as cells (with counts for aggregated social categories)

--- src/main/scala/eighties/h24/tools/PopulationGeopackageExporter.scala
+++ src/main/scala/eighties/h24/tools/PopulationGeopackageExporter.scala
@@ -2,9 +2,13 @@ package eighties.h24.tools
 
 import java.io.{File => JFile}
 import java.util
-
 import eighties.h24.generation._
-import eighties.h24.social.{Age, Education}
+import eighties.h24.social.AggregatedSocialCategory.shortAggregatedSocialCategoryIso
+import eighties.h24.social.{Age, AggregatedSocialCategory, Education}
+import eighties.h24.space
+import eighties.h24.space.{Location, generateWorld}
+import monocle.PLens
+import monocle.macros.Lenses
 import org.geotools.data.{DataUtilities, DefaultTransaction}
 import org.geotools.geometry.jts.{Geometries, ReferencedEnvelope}
 import org.geotools.geopkg.FeatureEntry
@@ -12,10 +16,13 @@ import org.geotools.referencing.CRS
 import org.locationtech.jts.geom.{Coordinate, GeometryFactory}
 import scopt.OParser
 
+import scala.util.Random
+
 object PopulationGeopackageExporter extends App {
   case class Config(
                      population: Option[JFile] = None,
-                     output: Option[JFile] = None
+                     output: Option[JFile] = None,
+                     cellExport: Boolean = false
                    )
 
   val builder = OParser.builder[Config]
@@ -30,7 +37,11 @@ object PopulationGeopackageExporter extends App {
       opt[java.io.File]('o', "output")
         .required()
         .action((x, c) => c.copy(output = Some(x)))
-        .text("output directory")
+        .text("output directory"),
+      opt[Boolean]('c', "cell")
+        .optional()
+        .action((x, c) => c.copy(cellExport = x))
+        .text("export as cells")
     )
   }
   OParser.parse(parser, args, Config()) match {
@@ -44,12 +55,23 @@ object PopulationGeopackageExporter extends App {
       val params = new util.HashMap[String, Object]()
       params.put("dbtype", "geopkg")
       params.put("database", outFile.getPath)
-      val specs = "geom:Point:srid=3035," +
-        "cellX:Integer," +
-        "cellY:Integer," +
-        "ageCat:String," +
-        "sex:String," +
-        "education:String"
+      val specs = if (config.cellExport) {
+        "geom:Polygon:srid=3035,pop:Integer," +
+          AggregatedSocialCategory.all.map(cat=>"pop_"+AggregatedSocialCategory.toCode(cat).mkString("_")+":Integer").mkString(",")
+//          "pop111:Integer,pop112:Integer,pop113:Integer," +
+//          "pop121:Integer,pop122:Integer,pop123:Integer," +
+//          "pop131:Integer,pop132:Integer,pop133:Integer," +
+//          "pop211:Integer,pop212:Integer,pop213:Integer," +
+//          "pop221:Integer,pop222:Integer,pop223:Integer," +
+//          "pop231:Integer,pop232:Integer,pop233:Integer"
+      } else {
+        "geom:Point:srid=3035," +
+          "cellX:Integer," +
+          "cellY:Integer," +
+          "ageCat:String," +
+          "sex:String," +
+          "education:String"
+      }
       val featureTypeName = "Population"
       val featureType = DataUtilities.createType(featureTypeName, specs)
       val geometryFactory = new GeometryFactory
@@ -73,24 +95,57 @@ object PopulationGeopackageExporter extends App {
       Log.log("create writer")
       val writer = geopkg.writer(entry,true, null, transaction)
       Log.log("Let's go")
-      // FIXME the index if for debug only: we might want to remove it later
-      for {
-        (feature, index) <- res.individualFeatures.zipWithIndex
-      } {
-        if (index % 100000 == 0) Log.log(s"$index")
-        import feature._
-        def point = geometryFactory.createPoint(new Coordinate(bbox.minI + location._1.toDouble * gridSize + gridSize / 2, bbox.minJ + location._2.toDouble * gridSize + gridSize / 2))
-        val values = Array[AnyRef](
-          point,
-          location._1.asInstanceOf[AnyRef],
-          location._2.asInstanceOf[AnyRef],
-          Age(ageCategory).toString.asInstanceOf[AnyRef],
-          (if (sex == 0) "Homme" else "Femme").asInstanceOf[AnyRef],
-          s"$education - ${Education(education).toString}".asInstanceOf[AnyRef]
-        )
-        def simpleFeature = writer.next
-        simpleFeature.setAttributes(values)
-        writer.write()
+      if (config.cellExport) {
+        @Lenses case class Individual(socialCategory: AggregatedSocialCategory, home: Short)
+        def homeV: PLens[Individual, Individual, (Int, Int), (Int, Int)] = Individual.home composeIso space.Location.indexIso
+        def buildIndividual(feature: IndividualFeature, random: Random) =
+          Individual(AggregatedSocialCategory(feature),Location.toIndex(feature.location))
+        def world = generateWorld(res.individualFeatures, buildIndividual, homeV, homeV, new util.Random(42))
+        val index = space.Index.indexIndividuals(world, homeV.get)
+        def getValue(individual: Individual) = individual.socialCategory
+        val mappedValues = index.cells.map(_.map(individuals => if (individuals.nonEmpty)
+          {
+            val categories = individuals.map(getValue)
+            Some(AggregatedSocialCategory.all.map(cat=>categories.count(c=> c == cat)))
+          } else None))
+        for {
+          (l, i) <- mappedValues.zipWithIndex
+          (valueOption, j) <- l.zipWithIndex
+          if valueOption.isDefined
+        } {
+          val ii = bbox.minI + i * gridSize
+          val jj = bbox.minJ + j * gridSize
+          val value = valueOption.get
+          val (p0, p1, p2, p3)  = (new Coordinate(ii, jj), new Coordinate(ii+gridSize, jj), new Coordinate(ii+gridSize, jj+gridSize), new Coordinate(ii, jj+gridSize))
+          def polygon = geometryFactory.createPolygon(Array(p0,p1,p2,p3,p0))
+          val values = Array[AnyRef](polygon, value.sum.asInstanceOf[AnyRef]) ++ value.map(_.asInstanceOf[AnyRef])
+          def simpleFeature = writer.next
+          simpleFeature.setAttributes(values)
+          writer.write()
+        }
+      } else {
+        // FIXME the index if for debug only: we might want to remove it later
+        for {
+          (feature, index) <- res.individualFeatures.zipWithIndex
+        } {
+          if (index % 100000 == 0) Log.log(s"$index")
+          import feature._
+          def point = geometryFactory.createPoint(new Coordinate(bbox.minI + location._1.toDouble * gridSize + gridSize / 2, bbox.minJ + location._2.toDouble * gridSize + gridSize / 2))
+
+          val values = Array[AnyRef](
+            point,
+            location._1.asInstanceOf[AnyRef],
+            location._2.asInstanceOf[AnyRef],
+            Age(ageCategory).toString.asInstanceOf[AnyRef],
+            (if (sex == 0) "Homme" else "Femme").asInstanceOf[AnyRef],
+            s"$education - ${Education(education).toString}".asInstanceOf[AnyRef]
+          )
+
+          def simpleFeature = writer.next
+
+          simpleFeature.setAttributes(values)
+          writer.write()
+        }
       }
       Log.log("close writer")
       writer.close()
